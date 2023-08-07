63e2c105ec1588b1736414bad6c21685d4a1495f
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-08-07 22:32:25 +0100

    add a few more exercises

--- week-1/errors/1.js
+++ week-1/errors/1.js
@@ -4,4 +4,4 @@ let lastName = "lloyd";
 
 const initials = ;
 
-// create a variable initials that stores the first 3 characters of each string in upper case the variables 
+// create a variable initials that stores the first 3 characters of each string in upper case the variables
---
+++ week-1/expressions/13.js
@@ -0,0 +1,11 @@
+let fraction = "3/4";
+
+let numerator;
+// log the value of numerator
+let denominator;
+console.log("denominator is ", denominator);
+// log the value of denominator
+
+let percentage;
+console.log("percentage is ", percentage);
+// log the value of percentage
