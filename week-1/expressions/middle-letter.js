1a40fb28cc5a65a60b1d1854fe00a0e724562929
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-09-05 13:48:25 +0100

    remove old exercises

--- week-1/expressions/middle-letter.js
+++
@@ -1,8 +0,0 @@
-const organisation = "Code Your Future!";
-const midPoint = organisation.length / 2;
-const middleLetter = organisation[midPoint];
-
-console.log(`The middle letter is ${middleLetter}`);
-
-// The console output should give the middle letter
-// At the moment, it is just giving the
