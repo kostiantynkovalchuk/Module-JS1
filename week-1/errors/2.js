af81c573709dea010d91f0304e0f27bfb9ae4c1b
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-09-09 11:02:39 +0100

    tidy up comments further

--- week-1/errors/2.js
+++ week-1/errors/2.js
@@ -1,4 +1,5 @@
-// Currently trying top print the string "I was born in Bolton" but it isn't working...
+// Currently trying to print the string "I was born in Bolton" but it isn't working...
+// what's the error ?
 
 console.log(`I was born in ${cityOfBirth}`);
 
--- week-1/exercises/initials.js
+++ week-1/exercises/initials.js
@@ -1,5 +1,5 @@
-let firstName = "Augusta";
-let middleName = "Ada";
-let lastName = "King";
+let firstName = "Creola";
+let middleName = "Katherine";
+let lastName = "Johnson";
 
 // Declare a variable called initials that stores the first 3 characters of each string in upper case
