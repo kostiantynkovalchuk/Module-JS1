2ccfc3af282db4333b916392016d475a7d5c960b
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-09-13 13:48:34 +0100

    tweak get angle type

--- week-3/implement/get-angle-type.js
+++ week-3/implement/get-angle-type.js
@@ -1,29 +1,25 @@
-// You will need to implement a function
+// You will need to implement a function getAngleType
 
 // Given an angle in degrees,
 // When the function getAngleType is called with this angle,
 // Then it should:
 
-// Check for Invalid Angle:
-// If the angle is less than 0 or greater than or equal to 360 degrees,
-// Then the function should return "Invalid angle."
-
 // Identify Right Angles:
 // If the angle is exactly 90 degrees,
-// Then the function should return "Right angle."
+// Then the function should return "Right angle"
 
 // Identify Acute Angles:
 // If the angle is less than 90 degrees,
-// Then the function should return "Acute angle."
+// Then the function should return "Acute angle"
 
 // Identify Obtuse Angles:
 // If the angle is greater than 90 degrees and less than 180 degrees,
-// Then the function should return "Obtuse angle."
+// Then the function should return "Obtuse angle"
 
 // Identify Straight Angles:
 // If the angle is exactly 180 degrees,
-// Then the function should return "Straight angle."
+// Then the function should return "Straight angle"
 // Identify Reflex Angles:
 
 // If the angle is greater than 180 degrees and less than 360 degrees,
-// Then the function should return "Reflex angle."
+// Then the function should return "Reflex angle"
