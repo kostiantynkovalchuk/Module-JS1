cb8a3bf0df35aa3151cb665db97167532bfa6fa0
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-09-13 14:23:48 +0100

    improve is valid triangle

--- week-3/implement/is-valid-triangle.js
+++ week-3/implement/is-valid-triangle.js
@@ -6,18 +6,19 @@
 // When the function isValidTriangle is called with these side lengths as input,
 // Then it should:
 
+// scenario: invalid triangle
+// Given the side lengths a, b, and c,
+// When the sum of any two side lengths is less than or equal to the length of the third side (i.e., a + b <= c, a + c <= b, b + c <= a),
+// Then it should return false because these conditions violate the Triangle Inequality, which states that the sum of the lengths of any two sides of a triangle must be greater than the length of the third side.
+
+// scenario: invalid triangle
 // Check for Valid Input:
 // Given the side lengths a, b, and c,
 // When any of the side lengths is less than or equal to zero,
-// Then it should return "Invalid" because a triangle cannot have zero or negative side lengths.
-// Check Triangle Inequality Theorem:
+// Then it should return false because a triangle cannot have zero or negative side lengths.
 
-// Given the side lengths a, b, and c,
-// When the sum of any two side lengths is less than or equal to the length of the third side (i.e., a + b <= c, a + c <= b, b + c <= a),
-// Then it should return "Invalid" because these conditions violate the Triangle Inequality, which states that the sum of the lengths of any two sides of a triangle must be greater than the length of the third side.
-// Valid Triangle:
-
+// scenario: valid triangle
 // Given valid side lengths that satisfy the conditions mentioned above,
 // When the function is called with these side lengths,
-// Then it should return "Valid" because the input forms a valid triangle.
+// Then it should return true because the input forms a valid triangle.
 // This specification outlines the behavior of the isValidTriangle function for different input scenarios, ensuring it properly checks for invalid side lengths and whether they form a valid triangle according to the Triangle Inequality Theorem.
