0aa3501b0641f53ca98431f6ead792b6b9e9dda7
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-09-13 13:48:48 +0100

    tweak is valid triangle

--- week-3/implement/is-valid-triangle.js
+++ week-3/implement/is-valid-triangle.js
@@ -1,7 +1,7 @@
-// Certainly! Here's a brief specification for a function that checks if a triangle is valid based on its side lengths using the "Given, When, Then" format:
+// Implement a function isValidTriangle
 
 // Given the lengths of three sides of a triangle (a, b, c),
-// When the function isTriangleValid is called with these side lengths as input,
+// When the function isValidTriangle is called with these side lengths as input,
 // Then it should:
 
 // Check for Valid Input:
@@ -13,10 +13,10 @@
 
 // Given the side lengths a, b, and c,
 // When the sum of any two side lengths is less than or equal to the length of the third side (i.e., a + b <= c, a + c <= b, b + c <= a),
-// Then it should return "Invalid" because these conditions violate the Triangle Inequality Theorem, which states that the sum of the lengths of any two sides of a triangle must be greater than the length of the third side.
+// Then it should return "Invalid" because these conditions violate the Triangle Inequality, which states that the sum of the lengths of any two sides of a triangle must be greater than the length of the third side.
 // Valid Triangle:
 
 // Given valid side lengths that satisfy the conditions mentioned above,
 // When the function is called with these side lengths,
 // Then it should return "Valid" because the input forms a valid triangle.
-// This specification outlines the behavior of the isTriangleValid function for different input scenarios, ensuring it properly checks for invalid side lengths and whether they form a valid triangle according to the Triangle Inequality Theorem.
+// This specification outlines the behavior of the isValidTriangle function for different input scenarios, ensuring it properly checks for invalid side lengths and whether they form a valid triangle according to the Triangle Inequality Theorem.
