eb93d858231c69732e9534ea203ad2ee1a755278
Sally McGrath <sally@codeyourfuture.io>
2023-09-20 18:07:10 +0100

    complete GWT, add expect to be
    sometimes then Then is missing from these specs

--- week-2/implement/to-pounds.js
+++ week-2/implement/to-pounds.js
@@ -1,6 +1,5 @@
-// In week-1, there is a program written in interpret/to-pounds.js
+// Find to-pounds.js in an earlier week in this repo
 
-// You will need to take this code and turn it into a reusable block of code.
-// You will need to declare a function called toPounds with an appropriately named parameter.
-
-// You should call this function a number of times to check it works for different inputs
+// Take this code and turn it into a reusable block of code.
+// Declare a function called toPounds with an appropriately named parameter.
+// Call this function a number of times to check it works for different inputs
