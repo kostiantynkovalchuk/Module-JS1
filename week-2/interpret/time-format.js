fc32ec45c99f680442a73fbc7916eb6c8ed53a5a
kostiantynkovalchuk <kostiantynkovalchuk32@gmail.com>
2024-10-24 18:41:50 +0200

    interpret resolved

--- week-2/interpret/time-format.js
+++ week-2/interpret/time-format.js
@@ -26,18 +26,24 @@ console.log(formatTimeDisplay(143));
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
+//3
 
 // Call formatTimeDisplay with an input of 143, now answer the following:
 
 // b) What value is assigned to the parameter num when pad is called for the first time?
+//totalHours % 24
 
 // c) What is the return value of pad when it is called for the first time?
+//`0${num}`
 
 // d) What is the value assigned to the parameter num when pad
 // is called for the last time in this program?  Explain your answer
+//seconds % 60, this is the value of remainingSeconds
 
 // e) What is the return value when pad is called
 // for the last time in this program?  Explain your answer
+//23 as this is a return value for input 143
 
 // f) Research an alternative way of padding the numbers in this code.
 // Look up the string functions on mdn
+//padStart(2, "0")
